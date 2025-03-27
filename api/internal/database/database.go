ca70c53ede436150a8affc05df6a7bbc7a38f6a5
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2025-03-27 12:07:06 +0100

    feat: :sparkles: Added migration and structs to models

--- api/internal/database/database.go
+++ api/internal/database/database.go
@@ -2,14 +2,49 @@ package database
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/MagnusHLund/VisitorCounter/internal/config"
+	"github.com/MagnusHLund/VisitorCounter/internal/models"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func NewDatabase(cfg *config.Config) (*gorm.DB, error) {
+	if err := createDatabaseIfNotExists(cfg); err != nil {
+		return nil, err
+	}
+
+	db, err := connectToDatabase(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := migrateDatabase(db); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func createDatabaseIfNotExists(cfg *config.Config) error {
+	dsnWithoutDB := fmt.Sprintf("%s:%s@tcp(%s:%s)/?charset=utf8mb4&parseTime=True&loc=Local",
+		cfg.DBUser, cfg.DBPass, cfg.DBHost, cfg.DBPort)
+	tempDB, err := gorm.Open(mysql.Open(dsnWithoutDB), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	createDBQuery := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", cfg.DBName)
+	if err := tempDB.Exec(createDBQuery).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func connectToDatabase(cfg *config.Config) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		cfg.DBUser, cfg.DBPass, cfg.DBHost, cfg.DBPort, cfg.DBName)
 
@@ -19,4 +54,16 @@ func NewDatabase(cfg *config.Config) (*gorm.DB, error) {
 	}
 
 	return db, nil
+}
+
+func migrateDatabase(db *gorm.DB) error {
+	if err := db.AutoMigrate(
+		&models.Visitor{},
+		&models.Page{},
+	); err != nil {
+		log.Println("Failed to migrate database: ", err)
+		return err
+	}
+
+	return nil
 }
--- api/internal/models/visitor.go
+++ api/internal/models/visitor.go
@@ -1,1 +1,10 @@
 package models
+
+type Visitor struct {
+	VisitorId       uint   `gorm:"primaryKey"`
+	PageId          uint   `gorm:"not null;uniqueIndex:idx_hashedip_pageid"`
+	TotalVisits     uint32 `gorm:"not null"`
+	FirstVisit      uint   `gorm:"not null"`
+	LatestVisit     uint   `gorm:"not null"`
+	HashedIpAddress string `gorm:"not null;uniqueIndex:idx_hashedip_pageid"`
+}
